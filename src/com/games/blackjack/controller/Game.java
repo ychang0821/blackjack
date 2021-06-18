8bf9896cfcb54ddf37fd438d378e4efade1e10a0
ychang0821 <changyun0821@gmail.com>
2021-06-18 09:25:16 -0700

    Add class Game, made Dealer and Player public classes. Moved Player into the right package

---
+++ src/com/games/blackjack/controller/Game.java
@@ -0,0 +1,10 @@
+package com.games.blackjack.controller;
+
+import com.games.blackjack.Dealer;
+import com.games.blackjack.Player;
+import java.util.List;
+
+class Game {
+  private Dealer dealer;
+  private List<Player> player;
+}
